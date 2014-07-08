9087b7472b9458315f2128310f5c7b8884bdab56
william mason <william.mason93@gmail.com>
2014-07-08 14:45:59 +0100

    added: another test

---
+++ src/apps/02_gluttest/main.cpp
@@ -0,0 +1,39 @@
+// -*- Mode:C++ -*-
+
+/**************************************************************************************************/
+/*                                                                                                */
+/* Copyright (C) 2014 University of Hull                                                          */
+/*                                                                                                */
+/**************************************************************************************************/
+/*                                                                                                */
+/*  module     :  src/apps/02_gluttest.cpp                                                        */
+/*  project    :                                                                                  */
+/*  description:                                                                                  */
+/*                                                                                                */
+/**************************************************************************************************/
+
+// includes, system
+
+#include <cstdlib>  // EXIT_SUCCESS
+
+// includes, project
+
+// #include <>
+
+// internal unnamed namespace
+
+namespace {
+  
+  // types, internal (class, enum, struct, union, typedef)
+
+  // variables, internal
+  
+  // functions, internal
+
+} // namespace {
+
+int
+main()
+{
+  return EXIT_SUCCESS;
+}
