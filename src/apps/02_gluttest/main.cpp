aa2fd207973c661cb477f6cf7d157033d96960cd
william mason <william.mason93@gmail.com>
2014-07-08 15:06:29 +0100

    added: glut hello world

--- src/apps/02_gluttest/main.cpp
+++ src/apps/02_gluttest/main.cpp
@@ -14,7 +14,8 @@
 
 // includes, system
 
-#include <cstdlib>  // EXIT_SUCCESS
+#include <GL/glut.h> // gl*
+#include <cstdlib>   // EXIT_SUCCESS
 
 // includes, project
 
@@ -30,10 +31,28 @@ namespace {
   
   // functions, internal
 
+  void
+  display()
+  {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+  }
+
 } // namespace {
 
 int
-main()
+main(int argc, char* argv[])
 {
+  glutInit(&argc, argv);
+
+  glutInitDisplayMode   (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitWindowSize    (500, 500);
+  glutInitWindowPosition(300, 200);
+  glutCreateWindow      ("Hello World!");
+  glutDisplayFunc       (&display);
+
+  glutMainLoop();
+
   return EXIT_SUCCESS;
 }
