90df338f090985d6481fa3f774dd6d4ecd7be4d0
william mason <william.mason93@gmail.com>
2014-07-11 14:41:41 +0100

    Added all necessary files to be able to run the BasicDemo

--- src/apps/03_glutandbullet/main.cpp
+++ src/apps/03_glutandbullet/main.cpp
@@ -14,9 +14,13 @@
 
 // includes, system
 
-#include <GL/glut.h>            // gl*
-#include <boost/filesystem.hpp> // boost::filesystem::path
-#include <cstdlib>              // EXIT_SUCCESS
+//#include <GL/glut.h>            // gl*
+//#include <boost/filesystem.hpp> // boost::filesystem::path
+//#include <cstdlib>              // EXIT_SUCCESS
+#include "BasicDemo.h"
+#include "GlutStuff.h"
+#include "btBulletDynamicsCommon.h"
+#include "LinearMath/btHashMap.h"
 
 // includes, project
 
@@ -32,58 +36,15 @@ namespace {
   
   // functions, internal
   
-  void
-  keyboard(unsigned char key, int x, int y) 
-  {
-    if (key == 'q') 
-    {
-      exit(EXIT_SUCCESS);
-    }
-  }
-  void 
-  reshape(int w, int h) 
-  {
-    float ratio = 1.0 * w/h;
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glViewport(0,0,w,h);
-    gluPerspective(45,ratio,1,1000);
-    glMatrixMode(GL_MODELVIEW);
-  }
-  void
-  idle()
-  {
-    //something
-    glutPostRedisplay();
-  }
-  void
-  display()
-  {
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    
-
-    glutSwapBuffers();
-  }
+ 
 
 } // namespace {
 
 int
 main(int argc, char* argv[])
 {
-  glutInit(&argc, argv);
-
-  glutInitDisplayMode   (GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-  glutInitWindowSize    (500, 500);
-  glutInitWindowPosition(300, 200);
-  glutCreateWindow      (boost::filesystem::path(argv[0]).filename().string().c_str());
-  glutDisplayFunc       (&display);
-  glutIdleFunc          (&idle);
-  glutReshapeFunc       (&reshape);
-  glutKeyboardFunc      (&keyboard);
-
-  glutMainLoop();
-
-  return EXIT_SUCCESS;
+  BasicDemo ccdDemo;
+  ccdDemo.initPhysics();
+  
+  return glutmain(argc, argv, 1024, 600, "Bullet physics demo", &ccdDemo);
 }
